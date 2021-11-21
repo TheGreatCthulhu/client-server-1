d4bf3d106024d34ab45a5d23778c3b0e0a191672
TheGreatCthulhu <94782198+TheGreatCthulhu@users.noreply.github.com>
2021-11-21 13:42:02 +0300

    initialcommit

---
+++ clientzach.cpp
@@ -0,0 +1,144 @@
+﻿// clientzach.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <winsock2.h>
+#include <thread>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+#include <string>
+
+#pragma comment(lib,"ws2_32.lib")
+#pragma warning(disable:4996) 
+
+#define MIN 0
+#define MAX 10000
+
+using namespace std;
+
+int Rand(int min, int max)
+{
+	return min + (std::rand() % (max - min + 1));
+}
+
+struct Command
+{
+	int number; //client number
+	int value; //his number to guess
+};
+
+enum EqualityType
+{
+	LESS,
+	EQUAL,
+	MORE
+};
+
+volatile int currentValue;
+int clientNumber;
+
+void sent(SOCKET m_socket, int value) {
+
+	Command cmd;
+	cmd.number = clientNumber;
+
+	cmd.value = value;
+
+	send(m_socket, (char*)&cmd, sizeof(Command), 0);
+}
+
+void Session(SOCKET m_socket)
+{
+	int answer = 0;
+
+  //Command answer;
+
+	int leftb = MIN;
+	int rightb = MAX;
+
+	while (1)
+	{
+		sent(m_socket, currentValue);
+
+		while (1)
+		{
+			/**/
+			
+			char recvbuf[sizeof(int)] = "";
+			int bytesRecv = recv(m_socket, recvbuf, sizeof(int), 0);
+			if (bytesRecv > 0)
+			{
+				answer = *(int*)recvbuf;
+				break;
+			}
+
+		}
+
+		if (answer == EQUAL)
+		{
+			break;
+			
+		}
+
+		if (answer == LESS)//less
+		{
+			leftb = currentValue;
+			currentValue = (leftb + rightb) / 2;
+
+		}
+		else //more
+		{
+			rightb = currentValue;
+			currentValue = (leftb + rightb) / 2;
+		}
+
+		Sleep(200);
+
+		cout << currentValue << endl;
+	}
+}
+
+int main()
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2),
+		&wsaData);
+	if (iResult != NO_ERROR)
+		printf("Error at WSAStartup()\n");
+
+	SOCKET m_socket;
+	m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (m_socket == INVALID_SOCKET)
+	{
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		WSACleanup();
+	}
+	else
+	{
+		int s = socket(PF_INET, SOCK_STREAM, 0);
+		struct sockaddr_in clientService;
+		clientService.sin_family = AF_INET;//con det
+		clientService.sin_addr.s_addr = inet_addr("127.0.0.1");
+		clientService.sin_port = htons(8080);
+
+		cout << "Enter client number" << endl;
+		cin >> clientNumber;
+
+		srand(std::time(0));
+		currentValue = Rand(MIN, MAX);
+
+		if (connect(m_socket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
+			printf("Failed to connect.\n");
+			WSACleanup();
+		}
+
+		std::thread th1(&Session, m_socket);
+
+		th1.join();
+
+		closesocket(m_socket);
+	}
+	_getch();
+	return 0;
+}
---
+++ serverzach.cpp
@@ -0,0 +1,124 @@
+﻿#include <stdio.h>
+#include <winsock2.h>
+#include <iostream>
+#include <conio.h>
+#include <signal.h>
+#include <thread>
+#include <process.h>
+#include <mutex>
+
+
+
+#define PORT 8080 
+#pragma comment(lib, "ws2_32.lib")
+
+#define MIN 0
+#define MAX 10000
+
+using namespace std;
+
+int Rand(int min, int max)
+{
+	return min + (std::rand() % (max - min + 1));
+}
+
+volatile int randomValue;
+bool hasWinner = false;
+
+std::mutex mtLock;
+
+struct Command
+{
+	int number; //client number
+	int value; //his number to guess
+};
+
+enum EqualityType
+{
+	LESS,
+	EQUAL,
+	MORE
+};
+
+unsigned int __stdcall ClientSession(void* m_socket) {
+	while (1)
+	{
+		char recvbuf[sizeof(Command)];
+		int bytesRecv = recv((SOCKET)m_socket, recvbuf, sizeof(Command), 0);
+		if (bytesRecv > 0)
+		{
+			Command* cmd = (Command*)recvbuf;
+
+			int answer;
+
+			if (cmd->value == randomValue)
+				answer = EQUAL;
+			else if (cmd->value > randomValue)
+				answer = MORE;
+			else if (cmd->value < randomValue)
+				answer = LESS;
+
+			send((SOCKET)m_socket, (char*)&answer, sizeof(int), 0);
+
+			if (answer == EQUAL)
+			{
+				mtLock.lock();
+				if (!hasWinner)
+				{
+					hasWinner = true;
+					cout << "We have a winner, client = " << cmd->number;
+				}
+				mtLock.unlock();
+
+				return 0;
+			}
+		}
+	}
+}
+
+int main(int argc, char const* argv[])
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+		printf("Error \n");
+
+	SOCKET m_socket;
+	m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);//trying to creat a socket
+	if (m_socket == INVALID_SOCKET)
+	{
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		WSACleanup();
+	}
+
+	
+	struct sockaddr_in sock_info;
+	sock_info.sin_family = AF_INET;
+	sock_info.sin_addr.s_addr = INADDR_ANY;
+	sock_info.sin_port = htons(PORT);
+
+	srand(std::time(0));
+	randomValue = Rand(MIN, MAX); //min max params
+	cout << randomValue << endl;
+
+	if (bind(m_socket, (SOCKADDR*)&sock_info, sizeof(sock_info)) == SOCKET_ERROR)
+	{
+		printf("bind() failed.\n");
+		closesocket(m_socket);
+	}
+
+	if (listen(m_socket, 1) == SOCKET_ERROR)
+		printf("Error listening on socket.\n");
+
+	SOCKET client_socket;
+	while ((client_socket = accept(m_socket, NULL, NULL))) {
+		// Create a new thread for the accepted client (also pass the accepted client socket).
+		unsigned threadID;
+		_beginthreadex(NULL, 0, &ClientSession, (void*)client_socket, 0, &threadID);
+	}
+
+	closesocket(m_socket);
+
+	_getch();
+	return 0;
+}
